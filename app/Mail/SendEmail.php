9547be05a89317df6098938f21556ae3618d71f8
hendisantika <hendisantika@yahoo.co.id>
2023-01-08 17:23:50 +0700

    #2 - Add SendEmail ✏️ 📗 🐳:email:

---
+++ app/Mail/SendEmail.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Testing Kirim Email')
+            ->view('emails.sendemail');
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Send Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
